1c1b7e5df3e635a13169aeea4a3096eb3e8edf8e
Paschee <mauricepaschke@yahoo.de>
2013-07-24 17:42:38 +0200

    Update PRB.cpp

--- PRB.cpp
+++ PRB.cpp
@@ -29,6 +29,8 @@ void __fastcall TForm1::FormCreate(TObject *Sender)
 	MaskEdit1->Text = "";
 	MaskEdit2->Text = "";
 	MaskEdit3->Text = "";
+	MaskEdit4->Text = "";
+	MaskEdit4->Color = clHighlight;
 	Application->MainFormOnTaskBar = false;
 }
 //---------------------------------------------------------------------------
@@ -40,55 +42,65 @@ void __fastcall TForm1::Button1Click(TObject *Sender)
 void __fastcall TForm1::Button3Click(TObject *Sender)
 {
 	/*Reboot*/
-	system("system -r -5");
-	Memo1->Lines->Append(">> Windows will reboot in 5 seconds.");
+	Memo1->Lines->Append(">> Windows will reboot now.");
+	system("shutdown.exe -r -t 0");
 }
 //---------------------------------------------------------------------------
 void __fastcall TForm1::Button4Click(TObject *Sender)
 {
 	/*Shutdown*/
-	system("system -s -5");
-	Memo1->Lines->Append(">> Windows will shudtown in 5 seconds.");
+	system("shutdown -s -t 0");
+	Memo1->Lines->Append(">> Windows will shudtown now.");
 }
 //---------------------------------------------------------------------------
 void __fastcall TForm1::Button2Click(TObject *Sender)
 {
-	/*Characterdetection*/
-	if (MaskEdit1->Text == MaskEdit2->Text && MaskEdit1->Text.Length() >= 4 && MaskEdit2->Text.Length() >= 4 && MaskEdit1->Text.Length() <= 128 && MaskEdit2->Text.Length() <= 128)
+	if (FileExists("Password.txt"))  // Damit neu anfangen !!!
 	{
-		/*If new file gets created, the old will be removed*/
-		if ("Password.txt" != 0)
+		/*Characterdetection*/
+		if (MaskEdit1->Text == MaskEdit2->Text && MaskEdit1->Text.Length() >= 4 && MaskEdit2->Text.Length() >= 4 && MaskEdit1->Text.Length() <= 128 && MaskEdit2->Text.Length() <= 128)
 		{
-			remove("Password.txt");
+			/*Create new Password File*/
+			PasswordCreateA = MaskEdit1->Text;
+			TTextWriter *Speichern = new TStreamWriter("Password.txt", false);
+			Speichern->WriteLine(MaskEdit1->Text);
+			Speichern->Free();
+
+			/*File attributes*/
+			// FileSetAttr("Password.txt", faHidden);
+			// FileSetAttr("Password.txt", faHidden);
+
+			/*Pseudo cmd positive result*/
+			Memo1->Lines->Append("File was safed and created succesfully");
+			Memo1->Lines->Append(">> You can now try to Log in !");
+			ShowMessage("Password committed succesfully!");
+		}
+		else
+		{
+			/*Pseudo cmd negative result*/
+			Memo1->Lines->Append("Creating failed.");
+			Memo1->Lines->Append(">> Click OK and repeat.");
+			ShowMessage("Creating the Password has failed - What was the problem ?\n\n  • Less than 4 characters\n  • More than 128 characters\n  • The 2nd Password isn't the same as the 1st\n  • Look out for big and small characters ");
 		}
 
-		/*Create new Password File*/
-		PasswordCreateA = MaskEdit1->Text;
-		TTextWriter *Speichern = new TStreamWriter("Password.txt", false);
-		Speichern->WriteLine(MaskEdit1->Text);
-		Speichern->Free();
+		/*ReadPassword
+		if ("Password.txt" != 0)
+		{
+			TTextReader *Open = new TStreamReader("Password.txt");
+			PasswordGet = Open->ReadLine();
+			Open->Free();
+		}
 
-		/*File attributes*/
-		FileSetAttr("Password.txt", faHidden);
-		FileSetAttr("Password.txt", faHidden);
-
-		/*Pseudo cmd positive result*/
-		Memo1->Lines->Append("File was safed and created succesfully");
-		Memo1->Lines->Append(">> You can now try to Log in !");
-		ShowMessage("Password committed succesfully!");
-	}
-	else
-	{
-		/*Pseudo cmd negative result*/
-		Memo1->Lines->Append("Creating failed.");
-		Memo1->Lines->Append(">> Click OK and repeat.");
-		ShowMessage("Creating the Password has failed - What was the problem ?\n\n  • Less than 4 characters\n  • More than 128 characters\n  • The 2nd Password isn't the same as the 1st\n  • Look out for big and small characters ");
-	}
+		if (MaskEdit1->Text == MaskEdit2->Text && MaskEdit4->Text == PasswordGet && MaskEdit1->Text.Length() >= 4 && MaskEdit2->Text.Length() >= 4 && MaskEdit1->Text.Length() <= 128 && MaskEdit2->Text.Length() <= 128)
+		{
+			Memo1->Lines->Append("Hallo");
+		}
+		*/
 }
 //---------------------------------------------------------------------------
 void __fastcall TForm1::Button5Click(TObject *Sender)
 {
-	/*Log in*/
+    /*Log in*/
 	ThePassword = MaskEdit3->Text;
 
 	/*Input Error*/
@@ -108,15 +120,15 @@ void __fastcall TForm1::Button5Click(TObject *Sender)
 
 		/*Compare input with safed data*/
 		if (PasswordGet == ThePassword)
-		{
-			Memo1->Lines->Append("Log in was succesful, enjoy your session");
-			ShowMessage("Press OK to enter yout computer.");
-			exit(1);
-		}
-		else
-		{
-			Memo1->Lines->Append("Log in was not succesful.");
-		}
+			{
+				Memo1->Lines->Append("Log in was succesful, enjoy your session");
+				ShowMessage("Press OK to enter yout computer.");
+				exit(1);
+			}
+			else
+			{
+				Memo1->Lines->Append("Log in was not succesful.");
+			}
 	}
 	else
 	{
